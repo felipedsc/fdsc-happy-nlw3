7b899abc1ae0398b31bb7e6e6abdaea261e7e823
Felipe Carvalho <felipe_fsc@hotmail.com>
2020-10-14 00:01:37 -0300

    web

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import mapMarkerImg from '../images/map-marker.svg';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import '../styles/pages/orphanages-map.css';
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+interface Props {
+
+}
+
+function OrphanagesMap(props: Props) {
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy" />
+
+                    <h2>Escolha um orfanato no mapa</h2>
+
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Vila Velha</strong>
+                    <span>Espírito Santo</span>
+                </footer>
+            </aside>
+
+            <Map
+                center={[-20.3562067, -40.278223]}
+                zoom={15}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </Map>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
+    )
+}
+
+export default OrphanagesMap;
